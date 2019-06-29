1fa65048d59574452493306dbff0de285fbae781
JamesGratrix <jamesgratrix@hotmail.co.uk>
2019-06-29 18:33:35 +0100

    Daycycle tweaking

--- BrainsEden19/Assets/TrafficJam/Scripts/DayCycleRotation.cs
+++ BrainsEden19/Assets/TrafficJam/Scripts/DayCycleRotation.cs
@@ -10,9 +10,28 @@ public class DayCycleRotation : MonoBehaviour
     [SerializeField]
     private float rotationalSpeed = 1.0f;
 
+    [SerializeField]
+    private Transform dayLightCool;
+    [SerializeField]
+    private Transform dayLightWarm;
+    [SerializeField]
+    private Transform NightLightCool;
+
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Vector3.zero, rotationDirection,Time.deltaTime * rotationalSpeed);
+        dayLightCool.Rotate(rotationDirection,Time.deltaTime * rotationalSpeed);
+        dayLightWarm.Rotate(rotationDirection, Time.deltaTime * rotationalSpeed);
+
+        if(dayLightCool.rotation.x > 180 || dayLightCool.rotation.x < 0)
+        {
+            dayLightCool.GetComponent<Light>().enabled = false;
+            dayLightWarm.GetComponent<Light>().enabled = false;
+        }
+        else
+        {
+            dayLightCool.GetComponent<Light>().enabled = true;
+            dayLightWarm.GetComponent<Light>().enabled = true;
+        }
     }
 }
