407baedc05f2a22677885d160c531ffa22aa7dab
Ian <cuffy94@gmail.com>
2019-06-30 15:15:09 +0100

    Auto stash before merge of "Code" and "origin/Code"

---
+++ BrainsEden19/Assets/TrafficJam/Scripts/ObstaclePreviewSpawn.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ObstaclePreviewSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] ObstaclePreviewPos;
+
+    private GameObject[] currentObstacle = new GameObject[4];
+
+    public Coroutine[] spawnCoros = new Coroutine[4];
+
+    [SerializeField]
+    Image[] previewImages;
+
+    [SerializeField]
+    float timerCooldown;
+
+    public void SpawnObstacle(GameObject obstacle, int playerIndex)
+    {
+        if (currentObstacle[playerIndex])
+        {
+            Destroy(currentObstacle[playerIndex]);
+        }
+
+        if (spawnCoros[playerIndex] == null)
+        {
+            spawnCoros[playerIndex] = StartCoroutine(Timer(obstacle, playerIndex));
+        }
+    }
+
+    IEnumerator Timer(GameObject obstacle, int playerIndex)
+    {
+        float step = 0.0f;
+        while(step < timerCooldown)
+        {
+            step += Time.deltaTime;
+            previewImages[playerIndex].fillAmount = step / timerCooldown;
+            yield return null;
+        }
+
+        currentObstacle[playerIndex] = Instantiate(obstacle, ObstaclePreviewPos[playerIndex].position, transform.rotation);
+        spawnCoros[playerIndex] = null;
+    }
+}
